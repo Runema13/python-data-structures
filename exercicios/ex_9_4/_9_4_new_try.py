b0f4b102b542ad6747fcaf741f2f75c0caf203cd
PAULO JORGE FLORINDO BARCIA <pjbarcia@hotmail.com>
2025-09-03 21:56:21 -0300

    Add files via upload

---
+++ exercicios/ex_9_4/_9_4_new_try.py
@@ -0,0 +1,37 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+with open(name) as handle: #sugestao do coach
+    lista = list()
+    for line in handle:
+        line = line.strip()
+        if line.startswith("From "):
+            words = line.split()
+            if len(words) > 1:
+                email = words[1] #adicionar um guardiao para linhas que só tenham From e nao tenham segunda palavra
+                lista.append(email)
+        
+dicionario = dict()
+for w in lista:
+    dicionario[w] = dicionario.get(w,0)+1
+    
+    
+value = None
+key = None
+for k,v in dicionario.items():
+    if value is None or v > value:
+        value = v
+        key = k
+print(f"{key} {value}")
+
+
+#9.4 Write a program to read through the mbox-short.txt and 
+# figure out who has sent the greatest number of mail messages. 
+#The program looks for 'From ' lines and 
+# takes the second word of those lines as the person who sent the mail. 
+#The program creates a Python dictionary that maps the sender's mail address to a
+# count of the number of times they appear in the file. 
+#After the dictionary is produced, the program reads through the dictionary using a
+# maximum loop to find the most prolific committer.
